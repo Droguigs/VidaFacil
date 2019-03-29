91589e1d205ae0a715c8cc8aa2e007c491020dc1
Douglas Schiavi <schiavi.douglas@gmail.com>
2019-03-29 02:28:19 -0300

    Layouts first tweaks

--- VidaFacil/ViewController.swift
+++ VidaFacil/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func loadNibs() {
+        collectionView.register(AboutCollectionViewCell.nib, forCellWithReuseIdentifier: AboutCollectionViewCell.identifier)
+        collectionView.register(LoginCollectionViewCell.nib, forCellWithReuseIdentifier: LoginCollectionViewCell.identifier)
+        collectionView.register(TableListCollectionViewCell.nib, forCellWithReuseIdentifier: TableListCollectionViewCell.identifier)
+    }
 
 }
 
+//MARK: COllection Delegate Methods
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TableListCollectionViewCell.identifier, for: indexPath) as! TableListCollectionViewCell
+            
+            _ = cell.contentView
+            cell.setupCell()
+            
+            return cell
+        case 1:
+            return collectionView.dequeueReusableCell(withReuseIdentifier: TableListCollectionViewCell.identifier, for: indexPath)
+        case 2:
+            return collectionView.dequeueReusableCell(withReuseIdentifier: TableListCollectionViewCell.identifier, for: indexPath)
+        default:
+            return UICollectionViewCell()
+        }
+    }
+}

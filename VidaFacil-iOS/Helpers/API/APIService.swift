f510e033cec31106b0c575b72ddf100a382c1189
Douglas Schiavi <schiavi.douglas@gmail.com>
2019-10-07 08:08:07 -0300

    ApiService modules update

--- VidaFacil-iOS/Helpers/API/APIService.swift
+++ VidaFacil-iOS/Helpers/API/APIService.swift
@@ -20,6 +20,18 @@ class ApiService: ApiServiceType {
         self.accessTokenCache = accessTokenCache
     }
     
+    func benefits(benefitId: Int?, completion: @escaping (Benefits?, Error?) -> ()) {
+        self.api.request(.benefits(benefitId: benefitId)) { [weak self] result in
+            self?.parse(result, type: Benefits.self, completion: { (result, error) in
+                if let _error = error {
+                    completion(nil, _error)
+                } else {
+                    completion(result, nil)
+                }
+            }, tokenExpired: {})
+        }
+    }
+    
     func categories(categoryId: Int?, completion: @escaping (Categories?, Error?) -> ()) {
         self.api.request(.categories(categoryId: categoryId)) { [weak self] result in
             self?.noTokenParse(result, type: Categories.self, completion: { (result, error) in
@@ -32,6 +44,18 @@ class ApiService: ApiServiceType {
         }
     }
     
+    func discountTypes(discountTypeId: Int?, completion: @escaping (DiscountTypes?, Error?) -> ()) {
+        self.api.request(.discountTypes(discountTypeId: discountTypeId)) { [weak self] result in
+            self?.parse(result, type: DiscountTypes.self, completion: { (result, error) in
+                if let _error = error {
+                    completion(nil, _error)
+                } else {
+                    completion(result, nil)
+                }
+            }, tokenExpired: {})
+        }
+    }
+    
     func establishments(establishmentId: Int?, categoryId: Int?, latitude: String?, longitude: String?, completion: @escaping (Establishments?, Error?) -> ()) {
         self.api.request(.establishments(establishmentId: establishmentId,
                                          categoryId: categoryId,
@@ -41,9 +65,69 @@ class ApiService: ApiServiceType {
         }
     }
     
+    func evaluations(evaluationId: Int?, evaluationType: String?, completion: @escaping (Evaluations?, Error?) -> ()) {
+        self.api.request(.evaluations(evaluationId: evaluationId, evaluationType: evaluationType)) { [weak self] result in
+            self?.parse(result, type: Evaluations.self, completion: { (result, error) in
+                if let _error = error {
+                    completion(nil, _error)
+                } else {
+                    completion(result, nil)
+                }
+            }, tokenExpired: {})
+        }
+    }
+    
     func login(user: String, password: String, completion: @escaping (LoginResult?, Error?) -> ()) {
         self.api.request(.login(user: user, password: password)) { [weak self] result in
             self?.parse(result, type: LoginResult.self, completion: completion, tokenExpired: {})
+        }
+    }
+    
+    func plans(planId: Int?, completion: @escaping (Plans?, Error?) -> ()) {
+        self.api.request(.plans(planId: planId)) { [weak self] result in
+            self?.parse(result, type: Plans.self, completion: { (result, error) in
+                if let _error = error {
+                    completion(nil, _error)
+                } else {
+                    completion(result, nil)
+                }
+            }, tokenExpired: {})
+        }
+    }
+    
+    func products(productId: Int?, completion: @escaping (Products?, Error?) -> ()) {
+        self.api.request(.products(productId: productId)) { [weak self] result in
+            self?.parse(result, type: Products.self, completion: { (result, error) in
+                if let _error = error {
+                    completion(nil, _error)
+                } else {
+                    completion(result, nil)
+                }
+            }, tokenExpired: {})
+        }
+    }
+    
+    func states(stateId: Int?, completion: @escaping (States?, Error?) -> ()) {
+        self.api.request(.states(stateId: stateId)) { [weak self] result in
+            self?.parse(result, type: States.self, completion: { (result, error) in
+                if let _error = error {
+                    completion(nil, _error)
+                } else {
+                    completion(result, nil)
+                }
+            }, tokenExpired: {})
+        }
+    }
+    
+    func users(userId: Int?, completion: @escaping (Users?, Error?) -> ()) {
+        self.api.request(.users(userId: userId)) { [weak self] result in
+            self?.parse(result, type: Users.self, completion: { (result, error) in
+                if let _error = error {
+                    completion(nil, _error)
+                } else {
+                    completion(result, nil)
+                }
+            }, tokenExpired: {})
         }
     }
     
--- VidaFacil-iOS/Helpers/API/APIServiceType.swift
+++ VidaFacil-iOS/Helpers/API/APIServiceType.swift
@@ -12,8 +12,15 @@ typealias Completion<T: RootApiModel> = (T?, Error?) -> ()
 
 protocol ApiServiceType {
     
+    func benefits(benefitId: Int?, completion: @escaping Completion<Benefits>)
     func categories(categoryId: Int?, completion: @escaping Completion<Categories>)
+    func discountTypes(discountTypeId: Int?, completion: @escaping Completion<DiscountTypes>)
     func establishments(establishmentId: Int?, categoryId: Int?, latitude: String?, longitude: String?, completion: @escaping Completion<Establishments>)
+    func evaluations(evaluationId: Int?, evaluationType: String?, completion: @escaping Completion<Evaluations>)
     func login(user: String, password: String, completion: @escaping Completion<LoginResult>)
+    func plans(planId: Int?, completion: @escaping Completion<Plans>)
+    func products(productId: Int?, completion: @escaping Completion<Products>)
+    func states(stateId: Int?, completion: @escaping Completion<States>)
+    func users(userId: Int?, completion: @escaping Completion<Users>)
     
 }
